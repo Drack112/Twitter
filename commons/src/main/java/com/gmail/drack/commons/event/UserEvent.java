1261b761c88e217af846bd95572c9cfd71656aa1
João Vitor <onepessoa3@gmail.com>
2024-10-08 15:46:51 -0300

    feat: event on update user data

---
+++ commons/src/main/java/com/gmail/drack/commons/event/UserEvent.java
@@ -0,0 +1,13 @@
+package com.gmail.drack.commons.event;
+
+
+public interface UserEvent {
+    Long getId();
+    String getFullName();
+    String getUsername();
+    String getAbout();
+    String getAvatar();
+    boolean isPrivateProfile();
+    boolean isMutedDirectMessages();
+    boolean isActive();
+}
