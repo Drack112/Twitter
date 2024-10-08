1251ea0103b02552d79d7b52fa9de712b8a1efd9
João Vitor <onepessoa3@gmail.com>
2024-10-08 15:47:02 -0300

    feat: new paths for auth routes

--- commons/src/main/java/com/gmail/drack/commons/constants/PathConstants.java
+++ commons/src/main/java/com/gmail/drack/commons/constants/PathConstants.java
@@ -10,4 +10,10 @@ public class PathConstants {
     public static final String USER = "/user";
     public static final String API_V1_USER = API_V1 + USER;
     public static final String UI_V1_USER = UI_V1 + USER;
+    public static final String SEARCH_USERNAME = "/items/search/{username}";
+
+    public static final String AUTH = "/auth";
+    public static final String UI_V1_AUTH = UI_V1 + AUTH;
+    public static final String REGISTRATION_CHECK = "/registration/check";
+
 }
