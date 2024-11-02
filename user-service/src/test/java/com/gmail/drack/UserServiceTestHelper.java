86d23a46709417aa3a6152abbda729293a9b6e02
João Vitor <onepessoa3@gmail.com>
2024-11-02 12:37:45 -0300

    test: api controller auth

---
+++ user-service/src/test/java/com/gmail/drack/UserServiceTestHelper.java
@@ -0,0 +1,56 @@
+package com.gmail.drack;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.projection.ProjectionFactory;
+import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
+
+import com.gmail.drack.commons.utils.TestConstants;
+import com.gmail.drack.repository.projection.AuthUserProjection;
+import com.gmail.drack.repository.projection.NotificationUserProjection;
+import com.gmail.drack.repository.projection.UserCommonProjection;
+
+public class UserServiceTestHelper {
+
+    private static final ProjectionFactory factory = new SpelAwareProxyProjectionFactory();
+    private static final PageRequest pageable = PageRequest.of(0, 20);
+
+    public static NotificationUserProjection createNotificationUserProjection() {
+        return factory.createProjection(
+                NotificationUserProjection.class,
+                Map.of(
+                        "id", 1L,
+                        "fullName", TestConstants.FULL_NAME,
+                        "username", TestConstants.USERNAME
+                ));
+    }
+
+    public static AuthUserProjection createAuthUserProjection() {
+        return factory.createProjection(
+                AuthUserProjection.class,
+                new HashMap<>() {{
+                    put("id", 1L);
+                    put("email", TestConstants.USER_EMAIL);
+                    put("fullName", TestConstants.FULL_NAME);
+                    put("username", TestConstants.USERNAME);
+                    put("location", TestConstants.LOCATION);
+                    put("about", TestConstants.ABOUT);
+                    put("website", TestConstants.WEBSITE);
+                    put("avatar", TestConstants.AVATAR_SRC_1);
+                }});
+    }
+
+    public static UserCommonProjection createUserCommonProjection() {
+        return factory.createProjection(
+                UserCommonProjection.class,
+                Map.of(
+                        "id", TestConstants.USER_ID,
+                        "email", TestConstants.USER_EMAIL,
+                        "fullName", TestConstants.FULL_NAME,
+                        "activationCode", TestConstants.ACTIVATION_CODE,
+                        "passwordResetCode", TestConstants.PASSWORD_RESET_CODE
+                ));
+    }
+}
