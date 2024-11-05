bdef7bdd956ba49cbbbf6187fbcae7603aa623d0
João Vitor <onepessoa3@gmail.com>
2024-11-05 18:55:10 -0300

    fix: test for AuthenticationAPI controller

--- user-service/src/test/java/com/gmail/drack/controller/api/AuthenticationApiControllerTest.java
+++ user-service/src/test/java/com/gmail/drack/controller/api/AuthenticationApiControllerTest.java
@@ -1,6 +1,6 @@
 package com.gmail.drack.controller.api;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
