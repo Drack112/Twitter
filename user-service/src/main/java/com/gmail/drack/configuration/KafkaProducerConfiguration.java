e14319873074e3510f28c8379f932313716e3d69
João Vitor <onepessoa3@gmail.com>
2024-11-07 11:11:52 -0300

    fix: kafka configuration with wrong serializer

--- user-service/src/main/java/com/gmail/drack/configuration/KafkaProducerConfiguration.java
+++ user-service/src/main/java/com/gmail/drack/configuration/KafkaProducerConfiguration.java
@@ -1,24 +1,23 @@
 package com.gmail.drack.configuration;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import com.gmail.drack.commons.event.*;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
-import com.gmail.drack.commons.event.SendEmailEvent;
-import com.gmail.drack.commons.event.UpdateUserEvent;
+import java.util.HashMap;
+import java.util.Map;
 
 @EnableKafka
 @Configuration
 public class KafkaProducerConfiguration {
+
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
@@ -31,16 +30,14 @@ public class KafkaProducerConfiguration {
         return props;
     }
 
-    
     @Bean
     public KafkaTemplate<String, UpdateUserEvent> kafkaUpdateUserTemplate() {
         return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(producerConfigs()));
     }
-
+    
     @Bean
     public KafkaTemplate<String, SendEmailEvent> kafkaSendEmailTemplate() {
         return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(producerConfigs()));
     }
-
 
 }
